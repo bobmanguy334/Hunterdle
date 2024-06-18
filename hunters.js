98f182ea04f11ffb7150c724cf9d1dd8a8ff85bf
bobmanguy334 <13491240+bobmanguy334@users.noreply.github.com>
2024-06-18 16:36:47 -0700

    Initial Commit

---
+++ hunters.js
@@ -0,0 +1,16 @@
+var hunter_list = [
+    ["Gon Freecss","Male",["Hunter Association"],["Enhancement"],"Black","Hunter Exam"],
+    ["Killua Zoldyck", "Male", ["Hunter Association"], ["Transmutation"],"White","Hunter Exam"],
+    ["Kurapika","Male",["Hunter Association", "Nostrade Family"],["Conjuration", "Specialization"],"Blonde","Hunter Exam"],
+    ["Leorio Paradinight","Male",["Hunter Association"],["Emission"],"Black","Hunter Exam"],
+    ["Tonpa","Male",["None"],["None"],"Brown","Hunter Exam"],
+    ["Satoz","Male",["Hunter Association"],["Unknown"],"Pale Lavender","Hunter Exam"],
+    ["Hisoka Morow","Male",["Hunter Association"],["Transmutation"],"Red","Hunter Exam"],
+    ["Menchi","Female",["Hunter Association"],["Unknown"],"Turquoise","Hunter Exam"],
+    ["Buhara","Male",["Hunter Association"],["Unknown"],"Black","Hunter Exam"],
+    ["Hanzo","Male",["Cloud-Hidden Style, Hunter Association"],["None"],"Bald","Hunter Exam"],
+    ["Beans","Male",["Hunter Association"],["Unknown"],"Bald","Hunter Exam"],
+    ["Isaac Netero","Male",["Hunter Association,Shingen-ryu Dojo"],["Enhancement"],"White","Hunter Exam"],
+    ["Lippo","Male",["Hunter Association,Trick Tower"],["Unknown"],"Violet","Hunter Exam"],
+    ["Pokkle","Male",["None"],["Unknown"],"Brown","Hunter Exam"],
+]
---
+++ hunterxhunterdle.js
@@ -0,0 +1,62 @@
+import "./hunters.js";
+
+hunter_list.forEach(addToGuessList)
+
+var answer = ["Pokkle","Male",["None"],["Unknown"],"Brown","Hunter Exam"]
+
+var guess_button = document.getElementById("guessButton")
+
+guess_button.addEventListener("click", function guessEvent() {
+    populateGuessCards(document.getElementById("guessDatalist").value)
+  }
+)
+
+function addToGuessList(item) {
+  var list = document.getElementById("guessDatalistOptions")
+  var option = document.createElement("option")
+  option.value=item[0]
+  list.appendChild(option)
+}
+
+function populateGuessCards(guess) {
+ for (let i = 0; i < hunter_list.length; i++)
+  {
+    if(hunter_list[i][0] == guess)
+      {
+        const table_row = document.getElementById("guessTable").appendChild(document.createElement("tr"))
+        table_row.setAttribute("class","guess-row-body")
+        for (let j = 0; j < hunter_list[i].length; j++)
+          {
+            createGuessCard(hunter_list[i][j], answer[j], j, table_row)
+          }
+      }
+  }
+}
+
+function createGuessCard(guessed_hunter, correct_hunter, card_num, row) {
+  const table_data = row.appendChild(document.createElement("td"))
+  const table_column = table_data.appendChild(document.createElement("div"))
+  const table_card = table_column.appendChild(document.createElement("div"))
+  const table_card_body = table_card.appendChild(document.createElement("div"))
+  const table_card_text = table_card_body.appendChild(document.createElement("div"))
+  table_column.setAttribute("class","col")
+  table_card.setAttribute("class","card")
+  if (card_num == 0) {table_card_body.setAttribute("class","card-body card-body-name")}
+  else if (card_num == 2 || card_num == 3) {
+    table_card_body.setAttribute("class","card-body card-body-fail")
+    for (let i = 0; i < guessed_hunter.length; i++) {
+      for (let j = 0; j < correct_hunter.length; j++) {
+        if (guessed_hunter[i] == correct_hunter[j]) {
+          table_card_body.setAttribute("class","card-body card-body-success")
+          break;
+        }
+      }
+    }
+  }
+  else {
+    if(correct_hunter != guessed_hunter) {table_card_body.setAttribute("class","card-body card-body-fail")}
+    else {table_card_body.setAttribute("class","card-body card-body-success")}
+  }
+  table_card_text.setAttribute("class","card-text")
+  table_card_text.innerHTML = guessed_hunter
+}
